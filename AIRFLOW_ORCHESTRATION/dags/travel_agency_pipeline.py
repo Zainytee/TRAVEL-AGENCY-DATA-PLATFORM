caddc9c3efc5c296d655e8331d25fe798dc6ecb3
Zainytee <61945334+Zainytee@users.noreply.github.com>
2024-11-24 21:13:21 +0100

    Update travel_agency_pipeline.py

--- AIRFLOW_ORCHESTRATION/dags/travel_agency_pipeline.py
+++ AIRFLOW_ORCHESTRATION/dags/travel_agency_pipeline.py
@@ -1,9 +1,9 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-from scripts.extract import extract_data,extract_data_s3,output_file
-from scripts.transform import transform_data,run_transformation
-from scripts.load import load_data,load_data_to_snowflake
+from scripts.extract import extract_data, extract_data_s3, output_file
+from scripts.transform import transform_data, run_transformation
+from scripts.load import load_data, load_data_to_snowflake
 
 # Default arguments for the DAG
 default_args = {
@@ -12,13 +12,12 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
 }
-#table name 
+# table name
 TABLE_NAME = "country_data"
 SCHEMA_NAME = "staging"
 SNOWFLAKE_CONN_ID = 'SNOWFLAKE_CONN_ID'
 BUCKET_NAME = "zainycap-bucket"
 KEY = "data/country_data_2024-11-18.parquet"
-
 # Define the DAG
 with DAG(
     dag_id="etl_pipeline_travel_agency",
@@ -28,7 +27,7 @@ with DAG(
     start_date=datetime(2024, 11, 17),
     catchup=False,
 ) as dag:
-
+    
     extract_task = PythonOperator(
         task_id="extract_data",
         python_callable=extract_data,
@@ -45,28 +44,30 @@ with DAG(
         task_id="load_data",
         python_callable=load_data,
         templates_dict={
-            "s3_key": "data/country_data_{{ ds }}.parquet",  # Jinja template for dynamic date substitution
+            # Jinja template for dynamic date substitution
+            "s3_key": "data/country_data_{{ ds }}.parquet",
         },
         provide_context=True,
     )
-    # Extract task2
+    # Extract task2(extraction from s3 Bucket)
     extract_task2 = PythonOperator(
         task_id="extract_data_to_snowflake",
         python_callable=extract_data_s3,
-        op_args=[BUCKET_NAME,KEY],  # Replace with actual values
-        op_kwargs={'output_path': output_file},  # Ensure the file is saved locally for transformation
+        op_args=[BUCKET_NAME, KEY],
+        op_kwargs={'output_path': output_file},
         provide_context=True
     )
-    #Transform_task2
+    # Transform_task2
     transform_and_load_task = PythonOperator(
-    task_id='run_data_transformation',
-    python_callable=run_transformation,
-    op_args=[
-        load_data_to_snowflake,  # Pass the load function
-        TABLE_NAME,              # Snowflake table name
-        SCHEMA_NAME,             # Snowflake schema name
-        SNOWFLAKE_CONN_ID        # Snowflake connection ID
-    ],
+        task_id='run_data_transformation',
+        python_callable=run_transformation,
+        op_args=[
+            load_data_to_snowflake,
+            TABLE_NAME,
+            SCHEMA_NAME,
+            SNOWFLAKE_CONN_ID
+        ],
     )
     # Task dependencies
-    extract_task >> transform_task >> load_task >> extract_task2 >> transform_and_load_task
+    extract_task >> transform_task >> load_task
+    load_task >> extract_task2 >> transform_and_load_task
